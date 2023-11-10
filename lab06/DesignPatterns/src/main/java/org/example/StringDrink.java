08fe3657ef3baec41779ead04d7e8c308b0fe19b
Tiago <tiagofccarvalho13@gmail.com>
2023-11-10 11:10:12 +0000

    Created StringDrink class and tests

---
+++ lab06/DesignPatterns/src/main/java/org/example/StringDrink.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class StringDrink {
+    String text_;
+
+    public StringDrink(String text) {
+        text_ = text;
+    }
+
+
+    public String getText() {
+        return text_;
+    }
+
+    public void setText(String text) {
+        text_ = text;
+    }
+
+
+}
---
+++ lab06/DesignPatterns/src/test/java/org/example/StringDrinkTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringDrinkTest {
+
+    @Test
+    public void stringDrink() {
+        StringDrink drink = new StringDrink("ABCD");
+        assertEquals("ABCD", drink.getText());
+        drink.setText("DCBA");
+        assertEquals("DCBA", drink.getText());
+    }
+
+}
