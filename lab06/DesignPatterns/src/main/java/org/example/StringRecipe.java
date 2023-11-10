12cbcc9b26ccf7d9b412830e382c4f3d1bc9d391
Tiago <tiagofccarvalho13@gmail.com>
2023-11-10 12:19:53 +0000

    Made StringRecipe class

--- lab06/DesignPatterns/src/main/java/org/example/StringRecipe.java
+++ lab06/DesignPatterns/src/main/java/org/example/StringRecipe.java
@@ -1,4 +1,20 @@
 package org.example;
 
-public class StringRecipe {
+import java.util.List;
+
+public class StringRecipe{
+
+    private List<StringTransformer> recipleList;
+    private StringDrink drink;
+
+    public StringRecipe(List<StringTransformer> list) {
+        this.recipleList = list;
+    }
+
+    public void mix(StringDrink drink) {
+        for (StringTransformer elem : recipleList) {
+            elem.execute(drink);
+        }
+    }
+
 }
