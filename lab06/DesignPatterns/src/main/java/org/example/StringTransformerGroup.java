6dd521e71a6f1719d716ea3c4a0048b6a4c2f69e
Tiago <tiagofccarvalho13@gmail.com>
2023-11-23 17:52:49 +0000

    Made StringTransformerGroup

---
+++ lab06/DesignPatterns/src/main/java/org/example/StringTransformerGroup.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.List;
+
+public class StringTransformerGroup  implements StringTransformer {
+    private List<StringTransformer> transformers;
+    public StringTransformerGroup(List<StringTransformer> transformers) {
+        this.transformers  = transformers;
+    }
+
+    @Override
+    public void execute(StringDrink drink) {
+        for (StringTransformer transformer : transformers) {
+            transformer.execute(drink);
+        }
+    }
+}
